a481e56f3cd8677d715499523a9ece19963fdea4
Elad Hirsch <eladh@jfrog.com>
2019-06-06 00:34:33 +0300

    add docker build support

--- deploy-to-prod.sh
+++ deploy-to-prod.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -o xtrace
 
+usage() {
+    echo "Deploy project to Production"
+    echo "Usage: $1 artifactory address"
+    echo "Usage: $2 docker repository name"
+    exit 1
+}
+
+if [ -z "$1" ] || [ -z "$2"  ]; then
+    usage
+fi
+
+
 SERVER=$1
 REPOSITORY=$2
-PASSWORD=$3
 
 cat << EOF > /etc/docker/daemon.json
 {
@@ -13,7 +24,7 @@ EOF
 
 sudo service docker restart
 
-echo ${PASSWORD} | docker login --username foo --password-stdin
+echo "password" | docker login --username admin --password-stdin
 
 docker stop docker-web-app  && docker rm $_
 echo  rmi $(docker images --filter=reference=${SERVER}/${REPOSITORY}/web-app:latest)
@@ -27,4 +38,4 @@ docker run -d --name docker-web-app -p 80:80 ${SERVER}/${REPOSITORY}/web-app:lat
 docker run -d --name docker-go-service -p 3000:3000 ${SERVER}/${REPOSITORY}/go-service:latest
 
 
-echo "Deploy Ok"
+echo "Deploy Done"
